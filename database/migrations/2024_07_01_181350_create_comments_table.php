b3a3d4bac70d9fd486c73209d82ca2d3deee267f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-07-01 23:46:03 +0530

    migrate tables

--- database/migrations/2024_07_01_181350_create_comments_table.php
+++ database/migrations/2024_07_01_181350_create_comments_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('body');
             $table->timestamps();
+    
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
